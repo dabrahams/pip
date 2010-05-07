561ddd94a44567601205667a49ef36a36315732a
Dave Abrahams <dave@boostpro.com>
2010-05-06 21:35:24 -0400

    Collect diagnostic info for buildbot debugging

--- ryppl/tests/test_ryppl.py
+++ ryppl/tests/test_ryppl.py
@@ -14,11 +14,16 @@ from cStringIO import StringIO
 here = Path(here)
 
 try:
+    print '*** importing distutils2'
     import distutils2
 except ImportError:
+    print '*** FAILED'
     sys.path.insert(0, here.folder.folder/'distutils2'/'src')
+    print '*** Updating path with', sys.path[0], 'and retrying'
     import distutils2
-    
+
+print '*** distutils2 ==', distutils2.__file__
+
 from distutils2.metadata import DistributionMetadata as METADATA
 
 def create_projects(env, **projects):
